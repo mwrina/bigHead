f18a6b5a4cdd3fe20d325755a4b00e5adfe1a512
mari <95420095+mwrina@users.noreply.github.com>
2024-06-10 21:51:41 -0300

    index responsivo

--- sidebar.js
+++ sidebar.js
@@ -1,13 +1,25 @@
-var state = "expanded";
-        //Check if navbar is expanded or minimized and handle 
-        $('#navbar-toggle').click(function() {
-            if (state == "expanded") {
-                $('.sidebar').css('margin-left', '-180px');
-                state = "minimized";
-            } else {
-                if (state == "minimized") {
-                    $('.sidebar').css('margin-left', '0px');
-                    state = "expanded";
-                }
-            }
-        })
+let aberto = true;
+
+closeNav();
+
+document.querySelector('#menu').addEventListener('click', () => {
+    if(aberto == false) {
+        openNav();
+    } else {
+        closeNav();
+    }
+})
+
+function openNav() {
+    document.querySelector(".mobileSidebar").style.width = "25%";
+    document.querySelector(".colLinks").style.display = "flex";
+    document.querySelector(".mobileSidebar").style.height = "25%";
+    aberto = !aberto;
+}
+  
+function closeNav() {
+    document.querySelector(".mobileSidebar").style.width = "fit-content";
+    document.querySelector(".colLinks").style.display = "none";
+    document.querySelector(".mobileSidebar").style.height = "fit-content";
+    aberto = !aberto;
+}
